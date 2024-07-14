91c0366db7891d42eeeba130029a3629a3bbd777
Ahmed Ali <ahmedalielian20@gmail.com>
2024-07-14 18:17:15 +0300

    add loading screen

--- src/components/LoadingScreen/LoadingScreen.jsx
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function LoadingScreen() {
   return (
-    <div className='h-screen w-screen flex justify-center items-center'>Loading.Screen</div>
-  )
+    <div className="h-screen w-screen flex justify-center items-center">
+      <div
+        class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+        role="status"
+      >.
+        <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          
+        </span>
+      </div>
+      
+    </div>
+  );
 }
